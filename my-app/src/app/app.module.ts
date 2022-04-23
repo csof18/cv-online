bd8a2181a2879f2cba823396915b8e809db5b91c
csof18 <sofiacorrado18@gmail.com>
2022-04-23 16:02:33 -0300

    commit inicial

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { HabilidadesurasBlandasComponent } from './componentes/habilidadesuras-blandas/habilidadesuras-blandas.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { HttpClientModule } from '@angular/common/http';
+@NgModule({
+  declarations: [
+    AppComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    HabilidadesurasBlandasComponent,
+    ProyectosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ my-app/src/app/componentes/habilidadesuras-blandas/habilidadesuras-blandas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-habilidadesuras-blandas',
+  templateUrl: './habilidadesuras-blandas.component.html',
+  styleUrls: ['./habilidadesuras-blandas.component.css']
+})
+export class HabilidadesurasBlandasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
